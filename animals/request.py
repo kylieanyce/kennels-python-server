0d018c48d503c5f129fcb9c8805f9f54eadda770
kylieanyce <kylieanyce@gmail.com>
2021-04-08 11:27:18 -0500

    created python server and animals dir

---
+++ animals/request.py
@@ -0,0 +1,33 @@
+ANIMALS = [
+    {
+      "id": 2,
+      "name": "Kit",
+      "breed": "Pit",
+      "customerId": 2,
+      "locationId": 1
+    },
+    {
+      "id": 3,
+      "name": "Clown",
+      "breed": "Hound-Mix",
+      "locationId": 2,
+      "customerId": 3
+    },
+    {
+      "id": 4,
+      "name": "Buddy",
+      "breed": "Terrier",
+      "customerId": 4,
+      "locationId": 1
+    },
+    {
+      "name": "Boogie",
+      "breed": "Weiner",
+      "locationId": 2,
+      "customerId": 4,
+      "id": 5
+    }
+]
+
+def get_all_animals():
+    return ANIMALS
