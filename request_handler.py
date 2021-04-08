7b468db6a98880d74b83103cfcd20f57929ee286
kylieanyce <kylieanyce@gmail.com>
2021-04-08 12:48:51 -0500

    added get single animal

--- request_handler.py
+++ request_handler.py
@@ -1,12 +1,32 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals
+from animals import get_all_animals, get_single_animal
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
 # work together for a common purpose. In this case, that
 # common purpose is to respond to HTTP requests from a client.
 class HandleRequests(BaseHTTPRequestHandler):
+    def parse_url(self, path):
+        # Just like splitting a string in JavaScript. If the
+        # path is "/animals/1", the resulting list will
+        # have "" at index 0, "animals" at index 1, and "1"
+        # at index 2.
+        path_params = path.split("/")
+        resource = path_params[1]
+        id = None
+
+        # Try to get the item at index 2
+        try:
+            # Convert the string "1" to the integer 1
+            # This is the new parseInt()
+            id = int(path_params[2])
+        except IndexError:
+            pass  # No route parameter exists: /animals
+        except ValueError:
+            pass  # Request had trailing slash: /animals/
+
+        return (resource, id)  # This is a tuple
 
     # Here's a class function
     def _set_headers(self, status):
@@ -28,20 +48,26 @@ class HandleRequests(BaseHTTPRequestHandler):
     def do_GET(self):
         # Set the response code to 'Ok'
         self._set_headers(200)
+        response = {}  # Default response
 
         # Your new console.log() that outputs to the terminal
         print(self.path)
 
+        # Parse the URL and capture the tuple that is returned
+        (resource, id) = self.parse_url(self.path)
+        
         # It's an if..else statement
-        if self.path == "/animals":
+        if resource == "animals":
             # In Python, this is a list of dictionaries
             # In JavaScript, you would call it an array of objects
-            response = get_all_animals()
-        else:
-            response = []
+            if id is not None:
+                response = f"{get_single_animal(id)}"
+
+            else:
+                response = f"{get_all_animals()}"
 
         # This weird code sends a response back to the client
-        self.wfile.write(f"{response}".encode())
+        self.wfile.write(response.encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
