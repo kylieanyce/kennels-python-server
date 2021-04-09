fdfb8b22213cb27088e36bb658dbf182bc2a81db
kylieanyce <kylieanyce@gmail.com>
2021-04-09 13:38:54 -0500

    added delete and create animal methods

--- request_handler.py
+++ request_handler.py
@@ -4,6 +4,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals
 from animals import get_single_animal
 from animals import create_animal
+from animals import delete_animal
 from locations import get_all_locations
 from locations import get_single_location
 from employees import get_all_employees
@@ -48,7 +49,7 @@ class HandleRequests(BaseHTTPRequestHandler):
     def do_OPTIONS(self):
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+        self.send_header('Access-Control-Allow-Methods','GET, POST, PUT, DELETE')
         self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
         self.end_headers()
 
@@ -64,7 +65,7 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         # Parse the URL and capture the tuple that is returned
         (resource, id) = self.parse_url(self.path)
-        
+
         # ANIMALS -------------------------------------------------
         if resource == "animals":
             # In Python, this is a list of dictionaries
@@ -82,7 +83,7 @@ class HandleRequests(BaseHTTPRequestHandler):
 
             else:
                 response = f"{get_all_locations()}"
-        
+
         # EMPLOYEES ------------------------------------------------
         if resource == "employees":
             if id is not None:
@@ -90,7 +91,7 @@ class HandleRequests(BaseHTTPRequestHandler):
 
             else:
                 response = f"{get_all_employees()}"
-        
+
         # CUSTOMERS -----------------------------------------------
         if resource == "customers":
             if id is not None:
@@ -101,9 +102,9 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         self.wfile.write(response.encode())
 
-
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
+
     def do_POST(self):
         self._set_headers(201)
         content_len = int(self.headers.get('content-length', 0))
@@ -127,19 +128,35 @@ class HandleRequests(BaseHTTPRequestHandler):
         # Encode the new animal and send in response
         self.wfile.write(f"{new_animal}".encode())
 
-
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
+
     def do_PUT(self):
         self.do_POST()
 
+    def do_DELETE(self):
+        # Set a 204 response code
+        self._set_headers(204)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
 # This function is not inside the class. It is the starting
 # point of this application.
+
+
 def main():
     host = ''
     port = 8088
     HTTPServer((host, port), HandleRequests).serve_forever()
+
 
 if __name__ == "__main__":
     main()
