9675783b5a1189d0a55bd78ceb3d1294a23ed562
kylieanyce <kylieanyce@gmail.com>
2021-04-16 14:48:22 -0500

    added get animals with location

--- animals/request.py
+++ animals/request.py
@@ -1,16 +1,15 @@
 import sqlite3
 import json
 from models import Animal
+from models import Location
 
 
 def get_all_animals():
     # Open a connection to the database
     with sqlite3.connect("./kennel.db") as conn:
-
         # Just use these. It's a Black Box.
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
-
         # Write the SQL query to get the information you want
         db_cursor.execute("""
         SELECT
@@ -19,29 +18,32 @@ def get_all_animals():
             a.breed,
             a.status,
             a.location_id,
-            a.customer_id
-        FROM animal a
+            a.customer_id,
+            l.name location_name,
+            l.address location_address
+        FROM Animal a
+        JOIN Location l
+            ON l.id = a.location_id
         """)
-
         # Initialize an empty list to hold all animal representations
         animals = []
-
         # Convert rows of data into a Python list
         dataset = db_cursor.fetchall()
-
         # Iterate list of data returned from database
         for row in dataset:
-
             # Create an animal instance from the current row.
             # Note that the database fields are specified in
             # exact order of the parameters defined in the
             # Animal class above.
-            animal = Animal(row['id'], row['name'], row['breed'],
-                            row['status'], row['location_id'],
-                            row['customer_id'])
+            animal = Animal(row['id'], row['name'], row['breed'], row['status'],
+                            row['location_id'], row['customer_id'])
 
+            # Create a Location instance from the current row
+            location = Location(row['id'], row['location_name'], row['location_address'])
+
+            # Add the dictionary representation of the location to the animal
+            animal.location = location.__dict__
             animals.append(animal.__dict__)
-
     # Use `json` package to properly serialize list as JSON
     return json.dumps(animals)
 
@@ -50,7 +52,6 @@ def get_single_animal(id):
     with sqlite3.connect("./kennel.db") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
-
         # Use a ? parameter to inject a variable's value
         # into the SQL statement.
         db_cursor.execute("""
@@ -63,16 +64,13 @@ def get_single_animal(id):
             a.customer_id
         FROM animal a
         WHERE a.id = ?
-        """, ( id, ))
-
+        """, (id, ))
         # Load the single result into memory
         data = db_cursor.fetchone()
-
         # Create an animal instance from the current row
         animal = Animal(data['id'], data['name'], data['breed'],
-                            data['status'], data['location_id'],
-                            data['customer_id'])
-
+                        data['status'], data['location_id'],
+                        data['customer_id'])
         return json.dumps(animal.__dict__)
 
 
@@ -91,7 +89,7 @@ def get_animals_by_location(location):
             a.customer_id
         from Animal a
         WHERE a.location_id = ?
-        """, ( location, ))
+        """, (location, ))
         animals = []
         dataset = db_cursor.fetchall()
         for row in dataset:
@@ -117,7 +115,7 @@ def get_animals_by_status(status):
             a.customer_id
         from Animal a
         WHERE a.status = ?
-        """, ( status, ))
+        """, (status, ))
         animals = []
         dataset = db_cursor.fetchall()
         for row in dataset:
@@ -140,7 +138,7 @@ def update_animal(id, new_animal):
                 location_id = ?,
                 customer_id = ?
         WHERE id = ?
-        """, (new_animal['name'], new_animal['breed'], 
+        """, (new_animal['name'], new_animal['breed'],
               new_animal['status'], new_animal['location_id'],
               new_animal['customer_id'], id, ))
         # Were any rows affected?
@@ -179,6 +177,3 @@ def delete_animal(id):
         DELETE FROM animal
         WHERE id = ?
         """, (id, ))
-
-
-
