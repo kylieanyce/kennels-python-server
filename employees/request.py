277eac0aefa29d44e36dcba741b557f8a7e37a9d
kylieanyce <kylieanyce@gmail.com>
2021-04-20 09:29:42 -0500

    commented out old code;' l ;

--- employees/request.py
+++ employees/request.py
@@ -78,28 +78,28 @@ def get_employees_by_location(location):
     return json.dumps(employees)
 
 
-def create_employee(employee):
-    max_id = EMPLOYEES[-1]["id"]
-    new_id = max_id + 1
-    employee["id"] = new_id
-    EMPLOYEES.append(employee)
-    return employee
+# def create_employee(employee):
+#     max_id = EMPLOYEES[-1]["id"]
+#     new_id = max_id + 1
+#     employee["id"] = new_id
+#     EMPLOYEES.append(employee)
+#     return employee
 
 
-def delete_employee(id):
-    employee_index = -1
-    for index, employee in enumerate(EMPLOYEES):
-        if employee["id"] == id:
-            employee_index = index
-    if employee_index >= 0:
-        EMPLOYEES.pop(employee_index)
+# def delete_employee(id):
+#     employee_index = -1
+#     for index, employee in enumerate(EMPLOYEES):
+#         if employee["id"] == id:
+#             employee_index = index
+#     if employee_index >= 0:
+#         EMPLOYEES.pop(employee_index)
 
 
-def update_employee(id, new_employee):
-    # Iterate the ANIMALS list, but use enumerate() so that
-    # you can access the index value of each item.
-    for index, employee in enumerate(EMPLOYEES):
-        if employee["id"] == id:
-            # Found the animal. Update the value.
-            EMPLOYEES[index] = new_employee
-            break
+# def update_employee(id, new_employee):
+#     # Iterate the ANIMALS list, but use enumerate() so that
+#     # you can access the index value of each item.
+#     for index, employee in enumerate(EMPLOYEES):
+#         if employee["id"] == id:
+#             # Found the animal. Update the value.
+#             EMPLOYEES[index] = new_employee
+#             break
